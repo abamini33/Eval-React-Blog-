5e14eb8ce1af1a710c167af8d090642d93344910
Merce213 <nassim-bgb69@gmx.fr>
2024-04-12 15:06:31 +0200

    fetch author with the posts

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -19,7 +19,7 @@ const PostDetailPage = () => {
 		<div>
 			<h2>{post.title}</h2>
 			<p>{post.body}</p>
-			{/* <p>Autheur : {post.author}</p> */}
+			<p>Auteur : {post.author}</p>
 			<h3>Commentaires</h3>
 			{/* {comments.map((comment) => (
 				<CommentItem key={comment.id} comment={comment} />
