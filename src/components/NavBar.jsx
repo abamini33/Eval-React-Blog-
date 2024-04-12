cd90e666b82a1ff0720c44d754ee96b79303b0ed
Merce213 <nassim-bgb69@gmx.fr>
2024-04-12 11:10:39 +0200

    init router pages + navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+
+const checkIsActive = ({ isActive }) => {
+	return isActive
+		? {
+				color: "orange",
+		  }
+		: {};
+};
+
+const NavBar = () => {
+	return (
+		<header>
+			<nav style={{ display: "flex", gap: "1rem" }}>
+				<NavLink style={checkIsActive} to="/" end>
+					Home
+				</NavLink>
+				<NavLink style={checkIsActive} to="/new-post">
+					Add Post
+				</NavLink>
+			</nav>
+		</header>
+	);
+};
+
+export default NavBar;
---
+++ src/pages/PostDetails.jsx
@@ -0,0 +1,9 @@
+const PostDetails = () => {
+	return (
+		<div>
+			<h1>PostDetail</h1>
+		</div>
+	);
+};
+
+export default PostDetails;
